130b15aa9574d819bc34fe7e565b50fae7906410
wpvspv <48439513+wpvspv@users.noreply.github.com>
2020-04-28 19:50:46 +0900

    Update gussian&edge.py

--- example_analysis/Gussian_blur&Edge/gussian&edge.py
+++ example_analysis/Gussian_blur&Edge/gussian&edge.py
@@ -15,9 +15,9 @@ height, width = image.shape[:2] # 이미지 높이, 너비
 
 gray_img = grayscale(image) # 흑백이미지로 변환
     
-blur_img = gaussian_blur(gray_img, 3) # Blur 효과
+blur_img = gaussian_blur(gray_img, 3) # Blur 효과 # 홀수만 가능하고 수가 클수록 edge가 적어짐
         
-canny_img = canny(blur_img, 70, 210) # Canny edge 알고리즘
+canny_img = canny(blur_img, 70, 210) # Canny edge 알고리즘 # 1:2나 1:3 비율을 추천함
 
 cv2.imshow('result',canny_img) # Canny 이미지 출력
 cv2.waitKey(0) 
